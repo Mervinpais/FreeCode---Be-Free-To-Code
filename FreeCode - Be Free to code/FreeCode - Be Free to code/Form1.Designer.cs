4b671824aef8329ccdac3164f5849d8e8d818641
Mervinpaismakeswindows14 <62794814+Mervinpais@users.noreply.github.com>
2021-07-14 12:21:11 +0400

    Add files via upload

---
+++ FreeCode - Be Free to code/FreeCode - Be Free to code/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace FreeCode___Be_Free_to_code
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(712, 464);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FreeCode - Be Free To Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(310, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Version 1.0.0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(417, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.richTextBox2.Location = new System.Drawing.Point(730, 73);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(412, 464);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(532, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1154, 549);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "FreeCode V1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ FreeCode - Be Free to code/FreeCode - Be Free to code/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FreeCode___Be_Free_to_code
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class Global
+        {
+            //public static string name;
+            //public static String Name { get; set; }
+            //public static String code1;
+            //public static String code2;
+            //public static int repeat_times;
+            //public static String store_code_say;
+            //public static String store_code_repeat;
+            //public static String repeat_store1;
+            public static String if_statement_contains;  //* This is where most of the variable are kept, its like a storage room where a commands takes a variable and
+            public static String var_string;             //* then keeps it back once its done.
+            public static Boolean var_bool;
+            public static String ask;
+            public static String func;
+            public static String variable1;
+            public static String variable2;
+            public static String variable3;
+            public static String variable4;
+            public static String variable5;
+            public static int num2;
+            public static int num1;
+            public static int Math_result;
+            /*NEW VARIABLES FOR THE IDE*/
+            public static String Code;
+            public static String Output_cache;
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string codeline;
+            Console.WriteLine("W14# Coding Terminal - Checker");
+            Console.WriteLine("==============================");            
+            codeline = richTextBox1.Text;
+            if (codeline.StartsWith("Say"))
+            {
+                richTextBox2.Text = codeline.Substring(4) + Environment.NewLine;
+            }
+            else if (codeline == "time")
+            {
+                string Date_and_time = DateTime.Now.ToString("dddd , MMM dd yyyy,hh:mm:ss");
+                Console.WriteLine(Date_and_time);
+            }
+            else if (codeline.Contains("math"))
+            {
+                Global.num1 = Convert.ToInt32(codeline.Substring(5, 1));
+                Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
+                if (codeline.Substring(6, 1) == "+")
+                {
+                    Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
+                    Global.Math_result = Global.num1 + Global.num2;
+                    richTextBox2.Text = Global.Math_result + ToString();
+                }
+                else if (codeline.Substring(6, 1) == "-")
+                {
+                    Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
+                    Global.Math_result = (Global.num1 - Global.num2);
+                    richTextBox2.Text = Global.Math_result + ToString();
+                }
+                else if (codeline.Substring(6, 1) == "*")
+                {
+                    Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
+                    Global.Math_result = Global.num1 * Global.num2;
+                    richTextBox2.Text = Global.Math_result + ToString();
+                }
+                else if (codeline.Substring(6, 1) == "/")
+                {
+                    Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
+                    Global.Math_result = Global.num1 / Global.num2;
+                    richTextBox2.Text = Global.Math_result + ToString();
+                }
+            }
+            //}        
+            //if (codeline.StartsWith("math"))
+            //{
+            //    Global.num1 = Convert.ToInt32(codeline.Substring(5, 1));
+            //    Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
+            //    if (codeline.Substring(6, 1) == "+")
+            //    {
+            //        Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
+            //        Global.Math_result = Global.num1 + Global.num2;
+            //        Console.WriteLine(Global.Math_result);
+            //    }
+            //    else if (codeline.Substring(6, 1) == "-")
+            //    {
+            //        Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
+            //        Global.Math_result = (Global.num1 - Global.num2);
+            //        Console.WriteLine(Global.Math_result);
+            //    }
+            //    else if (codeline.Substring(6, 1) == "*")
+            //    {
+            //        Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
+            //        Global.Math_result = Global.num1 * Global.num2;
+            //        Console.WriteLine(Global.Math_result);
+            //    }
+            //    else if (codeline.Substring(6, 1) == "/")
+            //    {
+            //        Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
+            //        Global.Math_result = Global.num1 / Global.num2;
+            //        Console.WriteLine(Global.Math_result);
+            //    }
+
+            //}
+            //if (codeline.StartsWith("read"))     //Removed Since it will just causes bugs(in the IDE)
+            //{
+            //    if (codeline.Substring(4) != "")
+            //    {
+            //        string readText = File.ReadAllText(codeline.Substring(5) + ".txt");  // Read the contents of the file
+            //        Console.WriteLine(readText);  // Output the content
+            //    }
+            //    else if (codeline.Substring(4) == "")
+            //    {
+            //        Console.WriteLine("Please make sure there is a .txt file in W14-Sharp >> W14-Sharp_coding_language >> bin >> Debug >> netcoreapp3.1");
+            //        Console.WriteLine("Please close this program, insert your file, then reopen this program and then type in 'read *file name*.txt'");
+            //    }
+            //}
+            else
+            {
+                richTextBox2.Text = "ERROR(not really but); did you type any code in to compile?";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "";
+        }
+    }
+}  
+
