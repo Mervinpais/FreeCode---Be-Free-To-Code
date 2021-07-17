7470801a4dd181d0579078a159770d939c94f1b5
Mervinpaismakeswindows14 <62794814+Mervinpais@users.noreply.github.com>
2021-07-17 15:19:14 +0400

    New Update
    New Bug Fix with the Say command and new w14# documentation

--- FreeCode - Be Free to code/FreeCode - Be Free to code/Form1.Designer.cs
+++ FreeCode - Be Free to code/FreeCode - Be Free to code/Form1.Designer.cs
@@ -35,6 +35,9 @@ namespace FreeCode___Be_Free_to_code
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -80,11 +83,14 @@ namespace FreeCode___Be_Free_to_code
             // 
             // richTextBox2
             // 
+            this.richTextBox2.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
             this.richTextBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox2.ForeColor = System.Drawing.SystemColors.Window;
             this.richTextBox2.Location = new System.Drawing.Point(730, 73);
+            this.richTextBox2.MaxLength = 1000;
             this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.richTextBox2.Size = new System.Drawing.Size(412, 464);
             this.richTextBox2.TabIndex = 4;
             this.richTextBox2.Text = "";
@@ -99,11 +105,46 @@ namespace FreeCode___Be_Free_to_code
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(630, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 41);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(730, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 37);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Open";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(933, 24);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(209, 38);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "W14# Documentry";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1154, 549);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.button1);
@@ -126,6 +167,9 @@ namespace FreeCode___Be_Free_to_code
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- FreeCode - Be Free to code/FreeCode - Be Free to code/Form1.cs
+++ FreeCode - Be Free to code/FreeCode - Be Free to code/Form1.cs
@@ -8,10 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace FreeCode___Be_Free_to_code
 {
-
     public partial class Form1 : Form
     {
         public Form1()
@@ -21,7 +24,36 @@ namespace FreeCode___Be_Free_to_code
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
+        }
+        public class Error_Codes
+        {
+            public static String Reading_No_File_Error = "0xr00001";
+            public static String Command_Not_Recognised = "0xcnr00001";
+        }
+        public class Read_error1 : Exception
+        {
+            //Overriding the Message property
+            public override string Message
+            {
+                get
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    return $"Please make sure there is a .txt file in W14-Sharp >> W14-Sharp_coding_language >> bin >> Debug >> netcoreapp3.1 \nPlease close this program, insert your file, then reopen this program and then type in 'read *file name*.txt' \n Error_Code: " + Error_Codes.Reading_No_File_Error + "\n Error_Name: READING_NO_FILE_ERROR \n";
+                }
+            }
+        }
+        public class Command_error1 : Exception
+        {
+            //Overriding the Message property
+            public override string Message
+            {
+                get
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    return $"Error is not recognised as an internal/external command. Error_Code:" + Error_Codes.Command_Not_Recognised + " Error_Name: COMMAND_NOT_RECOGNISED";
+                }
+            }
         }
         public class Global
         {
@@ -50,27 +82,92 @@ namespace FreeCode___Be_Free_to_code
             public static String Code;
             public static String Output_cache;
         }
+        public static class Check {
+   public static int CheckOccurrences(string str1, string pattern) {
+      int count = 0;
+      int a = 0;
+      while ((a = str1.IndexOf(pattern, a)) != -1) {
+         a += pattern.Length;
+         count++;
+      }
+      return count;
+   }
+}
+        static void python_test() //this is  a test to see if c# and python can work together to make the language have more features :D
+        {
+            var psi = new ProcessStartInfo();
+            psi.FileName = @"C:\Users\mervi\AppData\Local\Programs\Python\Python39\python.exe";
+            var script = @"C:\Users\mervi\source\repos\W14-Sharp\W14-Sharp_coding_language\Python_help\python-to-extend-w14-sharp.py";
 
+            psi.Arguments = $"\"{script}\"";
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+            var errors = "";
+            var results = "";
 
+            using (var process = Process.Start(psi))
+            {
+                errors = process.StandardError.ReadToEnd();
+                results = process.StandardOutput.ReadToEnd();
+            }
+            Console.WriteLine("The Results and Errors are here below");
+            Console.WriteLine("Results");
+            Console.WriteLine(results);
+            Console.WriteLine();
+            Console.WriteLine("Errors");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(errors);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
-
+            if (richTextBox1.Text.Contains("Say"))
+            {
+                string find = "Say";
+                if (richTextBox1.Text.Contains(find))
+                {
+                    var matchString = Regex.Escape(find);
+                    foreach (Match match in Regex.Matches(richTextBox1.Text, matchString))
+                    {
+                        richTextBox1.Select(match.Index, find.Length);
+                        richTextBox1.SelectionColor = Color.DodgerBlue;
+                        richTextBox1.Select(richTextBox1.TextLength, 0);
+                        richTextBox1.SelectionColor = richTextBox1.ForeColor;
+                    };
+                }
+                char find_char = '"';
+                if (richTextBox1.Text.Contains(find_char))
+                {
+                    var matchString = Regex.Escape(find_char.ToString());
+                    foreach (Match match in Regex.Matches(richTextBox1.Text, matchString))
+                    {
+                        richTextBox1.Select(match.Index, find.Length);
+                        richTextBox1.SelectionColor = Color.DarkOrange;
+                        richTextBox1.Select(richTextBox1.TextLength, 0);
+                        richTextBox1.SelectionColor = richTextBox1.ForeColor;
+                    };
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             string codeline;
             Console.WriteLine("W14# Coding Terminal - Checker");
             Console.WriteLine("==============================");            
             codeline = richTextBox1.Text;
-            if (codeline.StartsWith("Say"))
+            if (codeline.Contains("Say"))
             {
-                richTextBox2.Text = codeline.Substring(4) + Environment.NewLine;
+                codeline = codeline.Replace("Say", "");
+                richTextBox2.Text += codeline + Environment.NewLine;
             }
             else if (codeline == "time")
             {
                 string Date_and_time = DateTime.Now.ToString("dddd , MMM dd yyyy,hh:mm:ss");
-                Console.WriteLine(Date_and_time);
+                richTextBox2.Text += (Date_and_time) + Environment.NewLine;
             }
             else if (codeline.Contains("math"))
             {
@@ -78,28 +175,28 @@ namespace FreeCode___Be_Free_to_code
                 Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
                 if (codeline.Substring(6, 1) == "+")
                 {
-                    Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
                     Global.Math_result = Global.num1 + Global.num2;
-                    richTextBox2.Text = Global.Math_result + ToString();
+                    richTextBox2.Text += Global.Math_result + ToString() + Environment.NewLine;
                 }
                 else if (codeline.Substring(6, 1) == "-")
                 {
-                    Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
                     Global.Math_result = (Global.num1 - Global.num2);
-                    richTextBox2.Text = Global.Math_result + ToString();
+                    richTextBox2.Text += Global.Math_result + ToString() + Environment.NewLine;
                 }
                 else if (codeline.Substring(6, 1) == "*")
                 {
-                    Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
                     Global.Math_result = Global.num1 * Global.num2;
-                    richTextBox2.Text = Global.Math_result + ToString();
+                    richTextBox2.Text += Global.Math_result + ToString() + Environment.NewLine;
                 }
                 else if (codeline.Substring(6, 1) == "/")
                 {
-                    Global.num2 = Convert.ToInt32(codeline.Substring(7, 1));
                     Global.Math_result = Global.num1 / Global.num2;
-                    richTextBox2.Text = Global.Math_result + ToString();
+                    richTextBox2.Text += Global.Math_result + ToString() + Environment.NewLine;
                 }
+            }
+            if (codeline == "python")
+            {
+                python_test();
             }
             //}        
             //if (codeline.StartsWith("math"))
@@ -145,15 +242,63 @@ namespace FreeCode___Be_Free_to_code
             //        Console.WriteLine("Please close this program, insert your file, then reopen this program and then type in 'read *file name*.txt'");
             //    }
             //}
-            else
+            /*else
             {
                 richTextBox2.Text = "ERROR(not really but); did you type any code in to compile?";
-            }
+            } */
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             richTextBox2.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*saveDialog_message MessageBox = new saveDialog_message();
+            MessageBox.show("you clicked me","Save",saveDialog_message.MessageBoxButton.YesNo);*/
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "")
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.OpenFile()))
+                {
+                    sw.Write(richTextBox1.Text);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string text = System.IO.File.ReadAllText(openFileDialog1.FileName);
+                richTextBox1.Text = text;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            W14_Sharp_Doc Help_on_W14 = new W14_Sharp_Doc();
+            Help_on_W14.Show();
         }
     }
 }  
